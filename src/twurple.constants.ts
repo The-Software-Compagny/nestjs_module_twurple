54640d46271955d03b0311a07eccd112deb196da
Tacx <12997062+tacxou@users.noreply.github.com>
2024-06-19 21:19:23 +0200

    chore: Remove unused code and update dependencies

--- src/twurple.constants.ts
+++ src/twurple.constants.ts
@@ -1,5 +1,3 @@
-// noinspection JSUnusedGlobalSymbols
-
 export const TWURPLE_MODULE_CONNECTION = 'default'
 export const TWURPLE_MODULE_CONNECTION_API_TOKEN = 'TwurpleModuleConnectionApiToken'
 export const TWURPLE_MODULE_CONNECTION_CHAT_TOKEN = 'TwurpleModuleConnectionChatToken'
